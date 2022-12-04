dc6143b0dd5a0cd2232df88489bdd54a3610a4fe
yanakhorolska <yana.khorolska@protonmail.com>
2022-12-04 11:53:59 +0100

    add async

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { MdClose } from 'react-icons/md';
-import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
 import css from './Task.module.css';
+import { deleteTask, toggleCompleted } from 'redux/operations';
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => dispatch(deleteTask(task.id));
 
-  const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
 
   return (
     <div className={css.wrapper}>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
 // файл оголошення функцій-селекторів
-export const getTasks = state => state.tasks;
+export const getTasks = state => state.tasks.items;
+export const getIsLoading = state => state.tasks.isLoading;
+export const getError = state => state.tasks.error;
 export const getStatusFilter = state => state.filters.status;
